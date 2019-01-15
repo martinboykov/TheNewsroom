e41c1c2d855d08ab7a2583f75b828fb1cd2276d9
martinboykov <martinboykov@mail.bg>
2019-01-15 18:57:56 +0200

    adding CORS headers for development and in case of two origin deployment

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,18 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+// REMOVE CORSE HEADERS IF NOT REQUIRED IN CASE OF ONE ORIGIN (ONE_APP) DEPLOYMENT
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token');
+  next();
+});
+
 app.get('/api/articles', (req, res, next) => {
   const articles = [
     {
