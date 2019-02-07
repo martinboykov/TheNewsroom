207e99427b88b845961875e878780c106f990ef5
martinboykov <martinboykov@mail.bg>
2019-02-07 22:59:02 +0200

    frontend: adding foundational layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsideRightTrippleComponent } from './aside/aside-right-tripple/aside-right-tripple.component';
+import { PostDetailsComponent } from './posts/post-details/post-details.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideRightTrippleComponent,
+    PostDetailsComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/aside/aside-right-tripple/aside-right-tripple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-right-tripple',
+  templateUrl: './aside-right-tripple.component.html',
+  styleUrls: ['./aside-right-tripple.component.scss']
+})
+export class AsideRightTrippleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
