feec64777d2c7dca69283a1032913c1c97b41088
martinboykov <martinboykov@mail.bg>
2019-02-26 21:58:40 +0200

    main-nave with some navigation

--- src/app/header/dropdown.directive.ts
+++ src/app/header/dropdown.directive.ts
@@ -1,11 +1,29 @@
-import { Directive, HostBinding, HostListener} from '@angular/core';
-
+import { Directive, ElementRef, OnInit, HostListener, HostBinding, Renderer2 } from '@angular/core';
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+export class DropdownDirective implements OnInit {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+
+  }
+  clicked = false;
+
+  // longer version using native css
+  // -------------------------------------
+  @HostListener('click', ['$event'])
+  onClick(event) {
+    if (!this.clicked) {
+      this.renderer.addClass(this.elementRef.nativeElement, 'activated');
+      const nextSibling = this.renderer.nextSibling(this.elementRef.nativeElement);
+      this.renderer.addClass(nextSibling, 'open');
+      this.clicked = true;
+    } else {
+      this.renderer.removeClass(this.elementRef.nativeElement, 'activated');
+      const nextSibling = this.renderer.nextSibling(this.elementRef.nativeElement);
+      this.renderer.removeClass(nextSibling, 'open');
+      this.clicked = false;
+    }
+  }
+  ngOnInit() {
   }
 }
