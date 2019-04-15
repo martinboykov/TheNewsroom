ee8d7888280c76e8d71bbfefb6e14bda9bf53dbf
martinboykov <martinboykov@mail.bg>
2019-04-15 22:43:05 +0300

    feature/deploy-google-cloud: set envirenment.prod for google cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   // appUrl: 'http://localhost:3000', // for testing
-  appUrl: 'https://the-newsroom-mean-app.herokuapp.com/',
+  // appUrl: 'https://the-newsroom-mean-app.herokuapp.com/',
+  appUrl: 'https://thenewsroom-mean-app.appspot.com/',
   apiUrl: 'https://the-newsroom-mean-app.herokuapp.com/api',
   SLACK_WEBHOOK: 'https://hooks.slack.com/services/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',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   apiUrl: 'http://localhost:3000/api',
   SLACK_WEBHOOK: 'https://hooks.slack.com/services/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',
   // appUrl: 'https://the-newsroom-mean-app.herokuapp.com/',
+  // appUrl: 'https://thenewsroom-mean-app.appspot.com/',
   // apiUrl: 'https://the-newsroom-mean-app.herokuapp.com/api',
 };
 
