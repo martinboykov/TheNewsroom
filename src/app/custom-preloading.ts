1a9e6297429aa023f7404c17b82fcc5abdbf27f6
martinboykov <martinboykov@mail.bg>
2020-05-03 20:13:40 +0300

    update to angular 9

--- src/app/custom-preloading.ts
+++ src/app/custom-preloading.ts
@@ -1,6 +1,8 @@
 import { of, Observable } from 'rxjs';
 import { PreloadingStrategy, Route } from '@angular/router';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CustomPreloading implements PreloadingStrategy {
   preload(route: Route, preload: Function) {
     if (route.data && route.data.preload) {
